bd45905252fceb3dceb0a4b815a59f6dd05f7114
Nick <nstames0@gmail.com>
2019-02-11 20:54:06 -0500

    added server back again
    rewrote server to be up to web host's standard. runs on port 8000 when testing on localhost. added procfile for use on web host. replaced p5.js with minified version. and reconfigured folders to have better management.

--- server.js
+++ server.js
@@ -1,2 +1,13 @@
+/*********** SETUP WEB SERVER ************/
+var express = require('express');
+var app = express();
 
-console.log("Yolo amiright");
+let port = process.env.PORT;
+if (port == null || port == "") { // if on prod server, then run on the required port, otherwise run on 8000 test port
+  port = 8000;
+}
+var server = app.listen(port);
+
+app.use(express.static('public'));
+
+console.log("...Node server started...");
